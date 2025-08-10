a4cc3f25cb07acea20684db9694991a0990554d4
Philip Craig <philipjcraig@gmail.com>
2025-08-10 15:49:03 +1000

    Use `gimli::write::ConvertUnit` for DWARF transform
    This uses on `gimli` to provide the generic support for DWARF
    transformation. Most of the remaining transform code is specific to the
    needs of wasmtime.
    
    The overall behaviour is the same as the previous implementation. We
    build a graph of the DIE dependencies, and prune DIEs that don't have
    valid code ranges. Then we traverse the DIE tree again and transform the
    DIEs. However there are some differences.
    
    Previously, unresolved references to DIEs were stored in `PendingUnitRefs`
    and `PendingDebugInfoRefs`, then at the end of the transformation we
    went back and fixed up these references. The new behaviour is to
    instead reserve IDs for all of the DIEs in the dependency tree before
    doing the transformation. This avoids the need to fix up references
    later. It also allows us to correctly handle references in DWARF
    expressions. Doing this with the previous scheme would have been
    complicated. This doesn't currently matter for wasmtime because it
    doesn't handle references in expressions yet, but gimli needed to handle
    it in order to be correct in general.
    
    The DIE tree pruning is slightly different. We no longer add back-edges
    pointing to namespace DIEs or the root DIE. Previously, these back-edges
    caused the DIEs to be reachable in cases where they should not
    have been. In particular, this affects DW_TAG_variable DIEs for
    global variables. We can't currently translate globals correctly,
    so they were added without a valid DW_AT_location, and pulled in
    references to their DW_AT_type entries. In the future when global
    variables can be translated correctly, they can be included by calling
    `ReserveUnitSection::require_entry`.

--- crates/cranelift/src/debug/transform/line_program.rs
+++ crates/cranelift/src/debug/transform/line_program.rs
@@ -1,34 +1,13 @@
-use super::TransformError;
 use super::address_transform::AddressTransform;
 use crate::debug::Reader;
 use anyhow::Error;
-use gimli::{DebugLineOffset, UnitRef, write};
+use gimli::write;
 
 pub(crate) fn clone_line_program(
-    unit: UnitRef<Reader<'_>>,
-    comp_name: Option<Reader<'_>>,
+    mut transform: write::ConvertLineProgram<'_, Reader<'_>>,
     addr_tr: &AddressTransform,
-    out_encoding: gimli::Encoding,
-    out_strings: &mut write::StringTable,
-    out_line_strings: &mut write::LineStringTable,
-) -> Result<(write::LineProgram, DebugLineOffset, Vec<write::FileId>, u64), Error> {
-    let Some(program) = unit.line_program.clone() else {
-        return Err(TransformError("Valid line program not found").into());
-    };
-
-    let offset = program.header().offset();
-    let line_encoding = program.header().line_encoding();
-    let mut transform = write::LineConvert::new(
-        unit.dwarf,
-        program,
-        comp_name,
-        out_encoding,
-        line_encoding,
-        out_line_strings,
-        out_strings,
-    )?;
-
-    while let Some(write::LineConvertSequence {
+) -> Result<(write::LineProgram, Vec<write::FileId>), Error> {
+    while let Some(write::ConvertLineSequence {
         start,
         rows: saved_rows,
         ..
@@ -84,5 +63,5 @@ pub(crate) fn clone_line_program(
         transform.end_sequence(map.len as u64);
     }
     let (out_program, files) = transform.program();
-    Ok((out_program, offset, files, 0))
+    Ok((out_program, files))
 }
